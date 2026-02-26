b8de8f78dec533e4acb378cbf9da1b65380ee176
SpatLyu <lyu.geosocial@gmail.com>
2026-02-27 00:50:35 +0800

    change na value processing

--- inst/include/infotheo.hpp
+++ inst/include/infotheo.hpp
@@ -104,7 +104,10 @@ namespace InfoTheo
         const Pattern& p,
         PackedKey& key,
         bool NA_rm)
-    {
+    {   
+        // ---------- Pattern-level NA ----------
+        if (NA_rm && p.size() == 1 && p[0] == 0)
+            return false;
         key.blocks.clear();
 
         uint64_t cur = 0;
@@ -112,9 +115,6 @@ namespace InfoTheo
 
         for (uint8_t x : p)
         {
-            if (NA_rm && x == 0)
-                return false;
-
             uint64_t val = static_cast<uint64_t>(x & 0x3);
             cur |= (val << shift);
             shift += 2;
