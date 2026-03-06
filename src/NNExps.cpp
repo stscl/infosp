da305cf9db04a2f01ee9b7afa663d1faeaf9e36f
SpatLyu <lyu.geosocial@gmail.com>
2026-03-07 00:00:01 +0800

    export nn utility

--- src/NNExps.cpp
+++ src/NNExps.cpp
@@ -33,3 +33,32 @@ Rcpp::List RcppNN4Mat(
     // Return nb object (List in R side)
     return std2nb(neighbours);
 }
+
+// Wrapper function to compute the nearest neighbours for an input feature matrix subset
+// [[Rcpp::export(rng = false)]]
+Rcpp::List RcppNN4MatSub(
+    const Rcpp::NumericMatrix& mat,
+    const Rcpp::IntegerVector& lib,
+    const Rcpp::IntegerVector& pred,
+    int k,
+    std::string method = "euclidean",
+    bool include_self = false
+) {
+    // Convert Rcpp::NumericMatrix to std::vector<std::vector<double>>
+    int numRows = mat.nrow();
+    int numCols = mat.ncol();
+    std::vector<std::vector<double>> cppMat(numRows, std::vector<double>(numCols));
+
+    for (int r = 0; r < numRows; ++r) {
+        for (int c = 0; c < numCols; ++c) {
+            cppMat[r][c] = mat(r, c);
+        }
+    }
+
+    // Call the neighbpurbood function
+    std::vector<std::vector<size_t>> neighbours = NN::NN4Mat(
+        cppMat, static_cast<size_t>(std::abs(k)), method, include_self);
+
+    // Return nb object (List in R side)
+    return std2nb(neighbours);
+}
