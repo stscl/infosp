f9f914413f9da6f3c2ebeca7e4bb9eb2d01c40ce
SpatLyu <lyu.geosocial@gmail.com>
2026-03-13 00:00:02 +0800

    draft spe utility

--- src/SPE_API.cpp
+++ src/SPE_API.cpp
@@ -7,4 +7,14 @@
 #include "infotheo.hpp"
 #include "surd.hpp"
 
-double RcppSPE4Lattice
+double RcppSPE4Lattice(
+    const Rcpp::NumericMatrix& mat,
+    const Rcpp::IntegerVector& vars,
+    const Rcpp::List& nb,
+    int E = 3,
+    int tau = 1,
+    int style = 1,
+    bool relative = true,
+    double base = 2.0,
+    bool na_rm = true
+)
